3e3279944eab5aa3da141b77f9c22cb0bd4337b6
abydityadev <abyan.raditya@icloud.com>
2024-02-11 18:53:52 +0700

    pomotimer

--- app/Comingsoon.tsx
+++ app/Comingsoon.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from '@/lib/utils';
 import React from 'react'
 import type { ComponentProps } from "react";
 import { useEffect, useState } from "react";
@@ -57,9 +58,14 @@ export default function Comingsoon() {
 
     return (
         <div className='flex h-screen items-center justify-center'>
-            <h1 className='text-4xl sm:text-9xl font-black'>
-                <span className='bg-emerald-500 text-emerald-950 px-10'>Coming Soon</span>
-                {emoji}
+            <h1 className='text-4xl sm:text-9xl font-black relative'>
+                <span className='bg-emerald-500 text-emerald-950 px-10 border-4 sm:border-8 border-emerald-600 rounded-lg sm:rounded-2xl'>Coming Soon</span>
+                <span className={cn(
+                    'text-3xl sm:text-7xl absolute -top-14 -right-5 sm:-top-20 sm:-right-12 bg-amber-500 py-3 rounded-lg sm:rounded-2xl',
+                    'border-4 sm:border-8 border-amber-600 sm:px-0 px-2'
+                )}>
+                    {emoji}
+                </span>
             </h1>
         </div>
     )
